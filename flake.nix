f52357aa6a0e907b057826c770f8f3a4a5ae6de6
Kobayashi <contact@zelinf.net>
2022-11-12 19:07:14 +0800

    Add nix flake (#12)
    * build by nix
    
    * nix build musl by default

---
+++ flake.nix
@@ -0,0 +1,56 @@
+{
+  # This is a template created by `hix init`
+  inputs.haskellNix.url = "github:input-output-hk/haskell.nix";
+  inputs.nixpkgs.follows = "haskellNix/nixpkgs-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils, haskellNix }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "x86_64-darwin"
+        "aarch64-linux"
+        "aarch64-darwin"
+      ];
+
+    in
+    flake-utils.lib.eachSystem supportedSystems (system:
+      let
+        overlays = [
+          haskellNix.overlay
+          (final: prev: {
+            cabalHoogle = final.haskell-nix.project' {
+              src = ./.;
+              name = "cabal-hoogle";
+              compiler-nix-name = "ghc924"; # Version of GHC to use
+
+              shell = {
+                tools = {
+                  cabal = "latest";
+                  haskell-language-server = "latest";
+                };
+              };
+            };
+          })
+        ];
+        pkgs = import nixpkgs { inherit system overlays; inherit (haskellNix) config; };
+        flake = pkgs.cabalHoogle.flake {
+          crossPlatforms = p: pkgs.lib.optionals pkgs.stdenv.hostPlatform.isLinux [
+            p.musl64
+          ];
+        };
+      in
+      flake // {
+        legacyPackages = pkgs;
+        packages.default = flake.packages."x86_64-unknown-linux-musl:cabal-hoogle:exe:cabal-hoogle";
+      });
+
+  # --- Flake Local Nix Configuration ----------------------------
+  nixConfig = {
+    # This sets the flake to use the IOG nix cache.
+    # Nix should ask for permission before using it,
+    # but remove it here if you do not want it to.
+    extra-substituters = [ "https://cache.iog.io" ];
+    extra-trusted-public-keys = [ "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" ];
+    allow-import-from-derivation = "true";
+  };
+}
