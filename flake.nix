5516c9f37f440b935076df1018ba4e913cc08020
Kobayashi <zelin.feng@pm.me>
2026-01-09 13:24:57 -0500

    Cabal 3.16 Support (#28)
    * Update for Cabal 3.16
    
    Updates this package to allow for cabal 3.16
    
    The import update comes from the guidance here:
    https://github.com/haskell/cabal/blob/7da2987abb234a9710d290568ee90de37d5742e1/release-notes/Cabal-3.16.0.0.md#migration-guide
    
    Other minor updates:
     - Allow typed-process 0.2.13, which appears to be a [minor bugfix](https://github.com/fpco/typed-process/blob/9e08c32913d0bc52be149988328dd5ba374b938d/ChangeLog.md#02130)
     - Adds ghc 9.12 to the flake.nix to line up with the tests
     - nix-rfc-format of flake.nix
    
    * extract CPP to standalone functions; changelog
    
    ---------
    
    Co-authored-by: YourFin <src@patrickn.co>

--- flake.nix
+++ flake.nix
@@ -4,42 +4,66 @@
 
     flake-utils.url = "github:numtide/flake-utils";
   };
-  outputs = inputs@{ nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (system:
+  outputs =
+    inputs@{ nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
       let
         pkgs = import nixpkgs { inherit system; };
-        compilers = [ "ghc92" "ghc94" "ghc96" ];
-        defaultCompiler = "ghc94";
+        compilers = [
+          "ghc94"
+          "ghc96"
+          "ghc98"
+          "ghc910"
+          "ghc912"
+        ];
+        defaultCompiler = "ghc912";
 
-        mkShell = compiler: pkgs.mkShell {
-          buildInputs = with pkgs; [
-            zlib.dev
-          ];
-          packages = with pkgs; [
-            cabal-install
-            haskell.compiler.${compiler}
-            ormolu
-            miniserve
-          ] ++ (with haskell.packages.${compiler};
-            (lib.lists.optional (compiler != "ghc96") haskell-language-server) ++
-              [ cabal-fmt ghcid ]
-          );
-          shellHook = ''
-            export CABAL_DIR=$(pwd)/.cabal
-            export CABAL_CONFIG=$(pwd)/.cabal/config
-          '';
-        };
+        mkShell =
+          compiler:
+          pkgs.mkShell {
+            buildInputs = with pkgs; [
+              zlib.dev
+            ];
+            packages =
+              with pkgs;
+              [
+                cabal-install
+                haskell.compiler.${compiler}
+                ormolu
+                miniserve
+              ]
+              ++ (
+                with haskell.packages.${compiler};
+                (lib.lists.optional (
+                  !(builtins.elem compiler [
+                    "ghc96"
+                  ])
+                ) haskell-language-server)
+                ++ (lib.lists.optional (
+                  !(builtins.elem compiler [
+                    "ghc912"
+                  ])
+                ) cabal-fmt)
+                ++ [
+                  ghcid
+                ]
+              );
+            shellHook = ''
+              export CABAL_DIR=$(pwd)/.cabal
+              export CABAL_CONFIG=$(pwd)/.cabal/config
+            '';
+          };
       in
       {
         devShells = {
           default = mkShell defaultCompiler;
-        } // builtins.listToAttrs (
-          builtins.map
-            (compiler: {
-              name = compiler;
-              value = mkShell compiler;
-            })
-            compilers
+        }
+        // builtins.listToAttrs (
+          builtins.map (compiler: {
+            name = compiler;
+            value = mkShell compiler;
+          }) compilers
         );
       }
     );
--- src/Hoogle/Cabal/Command/Common.hs
+++ src/Hoogle/Cabal/Command/Common.hs
@@ -1,5 +1,6 @@
-{-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE RecordWildCards #-}
 
 module Hoogle.Cabal.Command.Common
   ( GlobalOptions (..),
@@ -16,10 +17,13 @@ import Distribution.Client.CmdErrorMessages (renderCannotPruneDependencies, repo
 import Distribution.Client.DistDirLayout (distDirectory)
 import Distribution.Client.NixStyleOptions
 import Distribution.Client.ProjectOrchestration
+import Distribution.Client.ProjectPlanning.Types (ElaboratedInstallPlan)
 import Distribution.Client.ScriptUtils
 import Distribution.Client.Setup (GlobalFlags, InstallFlags (..), defaultGlobalFlags)
+import Distribution.Client.TargetProblem (TargetProblem)
+import Distribution.Client.Types.SourcePackageDb (SourcePackageDb)
 import Distribution.Simple (OptimisationLevel (NoOptimisation))
-import Distribution.Simple.Setup (ConfigFlags (..), Flag (..), HaddockFlags (..))
+import Distribution.Simple.Setup (ConfigFlags (..), HaddockFlags (..), toFlag)
 import Distribution.Simple.Utils (die')
 import qualified Distribution.Verbosity as Verbosity
 import Options.Applicative
@@ -66,7 +70,7 @@ readContext ::
   [String] ->
   IO Context
 readContext GlobalOptions {..} targetStrings =
-  withContextAndSelectors RejectNoTargets Nothing flags targetStrings' globalFlags HaddockCommand $ \targetCtx ctx targetSelectors -> do
+  withContextAndSelectors' RejectNoTargets Nothing flags targetStrings' globalFlags HaddockCommand $ \targetCtx ctx targetSelectors -> do
     let targetAction = TargetActionBuild
 
     baseCtx <- case targetCtx of
@@ -74,13 +78,12 @@ readContext GlobalOptions {..} targetStrings =
       GlobalContext -> return ctx
       ScriptContext path exemeta -> updateContextAndWriteProjectFile ctx path exemeta
 
-    let verbosity = Verbosity.normal
-    buildCtx <- runProjectPreBuildPhase verbosity baseCtx $ \elaboratedPlan -> do
+    buildCtx <- runProjectPreBuildPhase defaultVerbosity baseCtx $ \elaboratedPlan -> do
       -- Interpret the targets on the command line as build targets
       -- (as opposed to say repl or haddock targets).
       targets <-
-        either (reportTargetProblems verbosity "build") return $
-          resolveTargets
+        either (reportTargetProblems defaultVerbosity "build") return $
+          resolveTargets'
             selectPackageTargets
             selectComponentTarget
             elaboratedPlan
@@ -95,7 +98,7 @@ readContext GlobalOptions {..} targetStrings =
       elaboratedPlan'' <-
         if buildSettingOnlyDeps (buildSettings baseCtx)
           then
-            either (die' verbosity . renderCannotPruneDependencies) return $
+            either (die' defaultVerbosity . renderCannotPruneDependencies) return $
               pruneInstallPlanToDependencies
                 (Map.keysSet targets)
                 elaboratedPlan'
@@ -114,34 +117,84 @@ readContext GlobalOptions {..} targetStrings =
     defaultFlags = defaultNixStyleFlags defaultBuildFlags
     flags =
       defaultFlags
-#if MIN_VERSION_Cabal(3,14,0)
-        { configFlags =
-            (configFlags defaultFlags)
-              { configOptimization = Flag NoOptimisation
-              , configCommonFlags =
-                (configCommonFlags (configFlags defaultFlags))
-                { setupDistPref = Flag (makeSymbolicPath _globalOptions_builddir)
-                }
-              },
-#else
-        { configFlags =
-            (configFlags defaultFlags)
-              { configOptimization = Flag NoOptimisation,
-                configDistPref = Flag _globalOptions_builddir
-              },
-#endif
+        { configFlags = setBuildDir _globalOptions_builddir . disableOptimization $ configFlags defaultFlags,
           haddockFlags =
             (haddockFlags defaultFlags)
-              { haddockHoogle = Flag True,
-                haddockHtml = Flag True,
-                haddockLinkedSource = Flag True,
-                haddockQuickJump = Flag True
+              { haddockHoogle = toFlag True,
+                haddockHtml = toFlag True,
+                haddockLinkedSource = toFlag True,
+                haddockQuickJump = toFlag True
               },
           installFlags =
             (installFlags defaultFlags)
-              { installDocumentation = Flag True
+              { installDocumentation = toFlag True
               }
         }
     targetStrings' :: [String]
     targetStrings' = if null targetStrings then ["all"] else targetStrings
     globalFlags = defaultGlobalFlags
+
+disableOptimization :: ConfigFlags -> ConfigFlags
+disableOptimization flags = flags {configOptimization = toFlag NoOptimisation}
+
+setBuildDir :: FilePath -> ConfigFlags -> ConfigFlags
+#if MIN_VERSION_Cabal(3,14,0)
+setBuildDir buildDir flags =
+  flags
+    { configCommonFlags =
+        (configCommonFlags flags)
+          { setupDistPref = toFlag $ makeSymbolicPath buildDir
+          }
+    }
+#else
+setBuildDir buildDir flags =
+  flags { configDistPref = toFlag buildDir }
+#endif
+
+withContextAndSelectors' ::
+  AcceptNoTargets ->
+  Maybe ComponentKind ->
+  NixStyleFlags a ->
+  [String] ->
+  GlobalFlags ->
+  CurrentCommand ->
+  ( TargetContext ->
+    ProjectBaseContext ->
+    [TargetSelector] ->
+    IO b
+  ) ->
+  IO b
+#if MIN_VERSION_Cabal(3,16,0)
+withContextAndSelectors' = withContextAndSelectors defaultVerbosity
+#else
+withContextAndSelectors' = withContextAndSelectors
+#endif
+
+resolveTargets' ::
+  ( forall k.
+    TargetSelector ->
+    [AvailableTarget k] ->
+    Either
+      (TargetProblem err)
+      [k]
+  ) ->
+  ( forall k.
+    SubComponentTarget ->
+    AvailableTarget k ->
+    Either (TargetProblem err) k
+  ) ->
+  ElaboratedInstallPlan ->
+  Maybe SourcePackageDb ->
+  [TargetSelector] ->
+  Either
+    [TargetProblem err]
+    TargetsMap
+#if MIN_VERSION_Cabal(3,16,0)
+-- resolveTargetsFromSolver is a drop-in replacement of resolveTargets. See https://github.com/haskell/cabal/commit/0f1f67cb97dca952123f262e7670a200a783acf4
+resolveTargets' = resolveTargetsFromSolver
+#else
+resolveTargets' = resolveTargets
+#endif
+
+defaultVerbosity :: Verbosity.Verbosity
+defaultVerbosity = Verbosity.normal
